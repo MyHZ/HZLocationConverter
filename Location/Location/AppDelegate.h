e61335ee33c3208cee929b6bf0309ad3ac9f6929
MyHZ <752818915@qq.com>
2017-04-08 14:05:22 +0800

    firstCommit
    firstCommit

---
+++ Location/Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Location
+//
+//  Created by DM on 2017/4/8.
+//  Copyright © 2017年 HZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Location/Location/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Location
+//
+//  Created by DM on 2017/4/8.
+//  Copyright © 2017年 HZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
