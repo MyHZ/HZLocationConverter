dd5f70a8f1fb45aecd06fc513ee4914e5e890c0f
huangzhen <huangzhen@wzdai.com>
2018-04-14 11:13:36 +0800

    删除测试代码

--- Location/Location/ViewController.m
+++ Location/Location/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-//#import "HZLocationConverter.h"
 
 @interface ViewController ()
 
@@ -18,22 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-//    CLLocationCoordinate2D wgsLoca = (CLLocationCoordinate2DMake( 31.146596,121.352525));
-//    CLLocationCoordinate2D masLoca = [HZLocationConverter transformFromWGSToGCJ:wgsLoca];
-//    NSDictionary *mas_Dic = @{
-//                              @"longitude":@(masLoca.longitude),
-//                              @"latitude":@(masLoca.latitude)
-//                              };
-//    NSLog(@"mas_Dic  %@",mas_Dic);
-//    
-//    CLLocationCoordinate2D baiduLoca = [HZLocationConverter transformFromGCJToBaidu:masLoca];
-//    NSDictionary *baidu_dic = @{
-//                                @"longitude":@(baiduLoca.longitude),
-//                                @"latitude":@(baiduLoca.latitude)
-//                                };
-//    NSLog(@"baidu_dic %@",baidu_dic);
-    
 }
 
 - (void)didReceiveMemoryWarning {
